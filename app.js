2a836acaeadf6b19d82212ac0fa36f304eab7011
Waleed-Rafi <waleed.rafi626@gmail.com>
2021-06-10 22:38:45 +0500

    Refactoring delete route

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const app = express();
 require("dotenv").config();
 require("./db/sql.js");
 
-app.use(express.json());
-
 const movieShow = require("./routes/movieShow");
+
+const cors = require("cors");
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
--- routes/movieShow.js
+++ routes/movieShow.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const DB = require("../db/sql");
+const { validationResult, check } = require("express-validator");
 
 router.get("/movies/all", async(req, res) => {
-    const sql = "SELECT * FROM movie_show;";
+    const sql = "SELECT * FROM movie_show ORDER BY MOV_ID DESC;";
     try {
         DB.query(sql, async(err, result) => {
             if (err) return res.json({ error: "Error: Unable to Fetch Data!" });
@@ -14,26 +15,47 @@ router.get("/movies/all", async(req, res) => {
     }
 });
 
-router.post("/movie/create", async(req, res) => {
-    //query for creating table, runs only for once
-    /*const sql = "CREATE TABLE movie_show(" +
-                                                        "MOV_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-                                                        "MOV_NAME VARCHAR(50) NOT NULL," +
-                                                        "MOV_LANG VARCHAR(20) NOT NULL," +
-                                                        "MOV_HOUR DATETIME NOT NULL DEFAULT NOW()," +
-                                                        "RELEASE_DATE DATETIME NOT NULL DEFAULT NOW());";
-                                            */
-    const sql = "INSERT INTO movie_show SET ? ";
-    try {
-        await DB.query(sql, req.body, async(err, result) => {
-            if (err)
-                return res.json({ error: "Error: Unable to Insert data try again!" });
-            return res.json({ success: "Successfully Inserted Data" });
-        });
-    } catch (error) {
-        return res.json({ error: "Error: Unable to Insert data try again!" });
+router.post(
+    "/movie/create", [
+        check("MOV_NAME", "Error: Movie Name is invalid").isLength({
+            min: 3,
+            max: 50,
+        }),
+        check("MOV_LANG", "Error: Movie Language is invalid").isLength({
+            min: 3,
+            max: 20,
+        }),
+        check("MOV_THUMBNAIL", "Error: Movie Thumbnail is invalid").isLength({
+            min: 20,
+            max: 255,
+        }),
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({ error: errors.errors[0].msg });
+        }
+        //query for creating table, runs only for once
+        // const sql = `CREATE TABLE movie_show(
+        // MOV_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        // MOV_NAME VARCHAR(50) NOT NULL,
+        // MOV_LANG VARCHAR(20) NOT NULL,
+        // MOV_THUMBNAIL VARCHAR(255) NOT NULL,
+        // MOV_HOUR TIME NOT NULL DEFAULT "03:00:00",
+        // MOV_DATE DATE NOT NULL DEFAULT (current_date())
+        // );`;
+        const sql = "INSERT INTO movie_show SET ? ";
+        try {
+            await DB.query(sql, req.body, async(err, result) => {
+                if (err)
+                    return res.json({ error: "Error: Unable to Insert data try again!" });
+                return res.json({ success: "Successfully Inserted Data" });
+            });
+        } catch (error) {
+            return res.json({ error: "Error: Unable to Insert data try again!" });
+        }
     }
-});
+);
 
 router.put("/movie/update", async(req, res) => {
     const sql = "UPDATE movie_show SET ? WHERE MOV_ID = ? ";
@@ -51,7 +73,7 @@ router.put("/movie/update", async(req, res) => {
 router.delete("/movie/delete", async(req, res) => {
     const sql = "DELETE FROM movie_show WHERE MOV_ID = ?";
     try {
-        await DB.query(sql, req.body.MOV_ID, async(err, result) => {
+        await DB.query(sql, req.query.mov_id, async(err, result) => {
             if (err) return res.json({ error: "Error: Unable to Delete!" });
             return res.json({ success: "Successfully Deleted!!" });
         });
